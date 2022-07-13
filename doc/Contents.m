5a749d7607bb8e5a8b2c417889a7bdbee3a2bb4d
Zheng Ran <72971318+ranzhengcode@users.noreply.github.com>
2022-07-13 19:40:57 +0800

    Add files via upload

---
+++ doc/Contents.m
@@ -0,0 +1,71 @@
+
+%{
+                          Glimpse of VELAS
+
+                    +-------- basic ------------ basic functions
+                    |
+                    +-------- doc -------------- doc files
+                    |
+                    +-------- drawing ---------- plot functions             
+                    |
+                    +-------- examples --------- examples files
+                    |
+      velas-1.0.0 --+-------- GUI -------------- GUI functions
+                    |
+                    +-------- properties ------- properties functions
+                    |
+                    +-------- velasGUI.m ------- main GUI
+                    |
+                    +-------- velasPlot.m ------ Independent plot
+                    |
+                    +--------- velasScript.m --- calc without GUI
+
+%}
+
+%{
+    basic folder:
+
+	char           - Convert to a character array.
+	double         - Convert to a double array.
+	string         - Convert to a string array.
+	strings        - Create string array with no characters.
+	cellstr        - Convert to a cell array of character vectors.
+	blanks         - Character vector of spaces.
+%}
+
+%{
+	drawing folder:
+
+	iscellstr      - True for cell array of character vectors.
+	ischar         - True for character array.
+	isstring       - True for string array.
+	isstrprop      - Check whether elements are of a specified category.
+%}
+
+%{
+	examples folder:
+
+	iscellstr      - True for cell array of character vectors.
+	ischar         - True for character array.
+	isstring       - True for string array.
+	isstrprop      - Check whether elements are of a specified category.
+%}
+
+%{
+	GUI folder:
+
+	iscellstr      - True for cell array of character vectors.
+	ischar         - True for character array.
+	isstring       - True for string array.
+	isstrprop      - Check whether elements are of a specified category.
+%}
+
+%{
+	properties folder:
+
+	iscellstr      - True for cell array of character vectors.
+	ischar         - True for character array.
+	isstring       - True for string array.
+	isstrprop      - Check whether elements are of a specified category.
+%}
+
---
+++ doc/property.m
@@ -0,0 +1,17 @@
+% For properties
+%{
+            properties — Pro(2*8)
+    
+                        Young   Compressibility  Shear   Poisson  Bulk    Pugh Ratio   Hardness   Fracture Toughness
+            3D mode:     1            1           1       1        1         1            1              1
+            2D mode:     1            1           1       1        1         1            1              1
+    
+            Young3D             — Pro(1,1),  Young2D             — Pro(2,1);
+            Compressibility3D   — Pro(1,2),  Compressibility2D   — Pro(2,2);
+            Shear3D             — Pro(1,3),  Shear2D             — Pro(2,3);
+            Poisson3D           — Pro(1,4),  Poisson2D           — Pro(2,4);
+            Bulk3D              — Pro(1,5),  Bulk2D              — Pro(2,5);
+            PughRatio3D         — Pro(1,6),  PughRatio2D         — Pro(2,6),
+            Hardness3D          — Pro(1,7),  Hardness2D          — Pro(2,7);
+            FractureToughness3D — Pro(1,8),  FractureToughness3D — Pro(2,8);
+%}
