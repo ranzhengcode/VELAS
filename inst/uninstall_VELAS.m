34cec4953bc8a733d750fbd53d2fb2a3aa511508
Zheng Ran <72971318+ranzhengcode@users.noreply.github.com>
2023-04-21 09:30:41 +0800

    Update uninstall_VELAS.m

--- inst/uninstall_VELAS.m
+++ inst/uninstall_VELAS.m
@@ -1,12 +1,21 @@
 % uninstall VELAS automatically using the [rvpath] function.
 try
-    rmpath(fullfile(pwd));
-    rmpath(fullfile(pwd,'basic'));
-    rmpath(fullfile(pwd,'doc'));
-    rmpath(fullfile(pwd,'drawing'));
-    rmpath(fullfile(pwd,'GUI'));
-    rmpath(fullfile(pwd,'mpapi'));
-    rmpath(fullfile(pwd,'properties'));
+    if isempty(which('velasGUI'))
+        rmpath(fullfile(pwd));
+        rmpath(fullfile(pwd,'basic'));
+        rmpath(fullfile(pwd,'drawing'));
+        rmpath(fullfile(pwd,'GUI'));
+        rmpath(fullfile(pwd,'mpapi'));
+        rmpath(fullfile(pwd,'properties'));
+    else
+        velaspath = fileparts(which('velasGUI'));
+        rmpath(fullfile(velaspath));
+        rmpath(fullfile(velaspath,'basic'));
+        rmpath(fullfile(velaspath,'drawing'));
+        rmpath(fullfile(velaspath,'GUI'));
+        rmpath(fullfile(velaspath,'mpapi'));
+        rmpath(fullfile(velaspath,'properties'));
+    end
     disp('VELAS uninstall finished!');
     disp('Thank you for your use and support! If you have any suggestions or comments on VELAS, please email us.');
     disp('Email: ranzheng@outlook.com');
