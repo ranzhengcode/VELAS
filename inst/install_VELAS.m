f261389c18b6b4be4a97d882dc37ff3312915008
Zheng Ran <72971318+ranzhengcode@users.noreply.github.com>
2023-04-21 09:29:55 +0800

    Update install_VELAS.m

--- inst/install_VELAS.m
+++ inst/install_VELAS.m
@@ -1,13 +1,22 @@
 
 % install VELAS automatically using the [addpath] function.
 try
-    addpath(fullfile(pwd));
-    addpath(fullfile(pwd,'basic'));
-    addpath(fullfile(pwd,'doc'));
-    addpath(fullfile(pwd,'drawing'));
-    addpath(fullfile(pwd,'GUI'));
-    addpath(fullfile(pwd,'mpapi'));
-    addpath(fullfile(pwd,'properties'));
+    if isempty(which('velasGUI'))
+        addpath(fullfile(pwd));
+        addpath(fullfile(pwd,'basic'));
+        addpath(fullfile(pwd,'drawing'));
+        addpath(fullfile(pwd,'GUI'));
+        addpath(fullfile(pwd,'mpapi'));
+        addpath(fullfile(pwd,'properties'));
+    else
+        velaspath = fileparts(which('velasGUI'));
+        addpath(fullfile(velaspath));
+        addpath(fullfile(velaspath,'basic'));
+        addpath(fullfile(velaspath,'drawing'));
+        addpath(fullfile(velaspath,'GUI'));
+        addpath(fullfile(velaspath,'mpapi'));
+        addpath(fullfile(velaspath,'properties'));
+    end
     savepath;
     disp('VELAS installation completed. Enjoy!');
     disp('Please don''t hesitate to contact us if you have any questions about using VELAS or suggestions for improving VELAS.');
@@ -15,4 +24,6 @@ try
 catch
     disp('There is a problem with the [addpath] function. Please add the path manually according to the VELAS Manual.');
 end
+
+
 
